68e58e65b2388e0cdc0c1fe1ebb54a41dc650287
cjfiscus <cjfiscus@ucdavis.edu>
2014-02-19 12:02:44 -0800

    annotated

--- hapmap_to_structuretxt.R
+++ hapmap_to_structuretxt.R
@@ -4,9 +4,9 @@ xm=as.matrix(x[,c(1,12:length(x))]) # turns data frame into matrix, but skips co
 
 y<-t(as.matrix(xm)) # rotates the matrix
 
-letters=c("AA","CC","GG","TT","NN") # define lettered genotypes
+letters=c("AA","CC","GG","TT","NN") # define lettered genotypes (NN is missing)
 
-numbers=c(11,22,33,44,99) # define new genotypes
+numbers=c(11,22,33,44,99) # define new genotypes (99 for missing)
 
 for( i in 1:5){ y[which(y==letters[i])]=numbers[[i]]} # for loop that replaces all AA with 11, CC with 22, etc.
 
